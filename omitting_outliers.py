cc1a8db077d83d3b647721dda31f7d1798a9e98e
sdas1 <sdas@uni-osnabrueck.de>
2021-05-11 06:52:06 +0200

    Update omitting_outliers.py

--- omitting_outliers.py
+++ omitting_outliers.py
@@ -4,7 +4,7 @@ def remove_outliers(data, m=3, replace=True):
     # implement task 1 here
     raise NotImplementedError
 
- '''
+"""
 def remove_outliers(an_array, m=3, replace=True):
     mean = np.mean(an_array)
     sigma = np.std(an_array)
@@ -21,6 +21,6 @@ if __name__ == "__main__":
 
     data = np.array([10, 10, 10, 17, 10, 10])
     print(remove_outliers(data, m=3, replace=False))
-  '''
+"""
 
 
